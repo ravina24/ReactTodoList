4fd489980ddf76aaf0d0a06834c681293ef2abe3
Ravina <ravinakaur@hotmail.com>
2019-06-01 16:09:25 -0700

    Changed component to stateful class component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import TodoItem from "./components/TodoItem.js";
 import todosData from "./components/todosData.js";
 
-function App() {
 
-  const todoList = todosData.map(item => <TodoItem key={item.id} item={item} />);
+class App extends React.Component {
 
-  return (
-      <div className="todoList">
-        {todoList}
-      </div>
-    );
+	constructor() {
+		super();
+		this.state = {
+			data: todosData
+		}
+	}
+
+	render() {
+		const todoList = this.state.data.map(item => <TodoItem key={item.id} item={item} />);
+
+		 return (
+		     <div className="todoList">
+		       {todoList}
+		     </div>
+		);
+	}
 }
 
 export default App;

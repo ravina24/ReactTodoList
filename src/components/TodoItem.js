8f0306c0ee60a81ac35fdaeb842bc0459ce69cb2
Ravina <ravinakaur@hotmail.com>
2019-06-01 16:40:59 -0700

    Added onChange handler to checkbox input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const TodoItem = (props) => {
 	return (
 			<div className="todo-item">
-				<input type="checkbox" id={props.item.id} checked={props.item.completed} /> 
+				<input type="checkbox" id={props.item.id} checked={props.item.completed} onChange={() => alert("Tried to change")} /> 
 			    <label for="checkbox">{props.item.text}</label>
 			    <br />
 			</div>
